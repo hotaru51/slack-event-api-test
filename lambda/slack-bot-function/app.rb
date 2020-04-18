53a1ff54aa66510305b94c67f72a7e497e95404c
hotaru <36354847+hotaru51@users.noreply.github.com>
2020-04-18 22:02:26 +0900

    Event Subscription対応

--- lambda/slack-bot-function/app.rb
+++ lambda/slack-bot-function/app.rb
@@ -1,5 +1,10 @@
 require 'json'
+require 'pp'
 
 def handler(event:, context:)
-  {statusCode: 200, headers: {"Content-Type": "application/json"}, body: JSON.generate(event)}
+  print JSON.dump(event)
+  event_body = JSON.parse(event['body'])
+  print JSON.dump(event_body)
+  res_body = {challenge: event_body['challenge']}
+  {statusCode: 200, headers: {"Content-Type": "application/json"}, body: JSON.generate(res_body)}
 end
